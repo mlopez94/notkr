8c6a58fda2b96162141471f677194ec1771338bf
Matthew Lopez <mlopez@domain.local>
2021-07-01 16:49:31 -0400

    connected pages to express

--- server.js
+++ server.js
@@ -6,16 +6,25 @@ const PORT = process.env.PORT || 3001;
 const app = express(); // initiate server
 
 // parse incoming string to array
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // incoming JSON data
 app.use(express.json());
 
 app.use(express.static("public")); // heroku will display static pages from public folder
 
-app.get('/api/animals', (req, res) => {
-    res.send('Hello!');
-  });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html')); // index page
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));// notes page
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html')); // index
+});
 
 
 
